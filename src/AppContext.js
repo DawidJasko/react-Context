807ec9287d8617392aeceb178af05e2685007d08
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-11-21 18:27:17 +0100

    2 files modified, add AppProvider

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,8 +1,24 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-export const defaultObject = {
-  isUserLogged: false,
-  toggleLoggedState: () => console.log('Domyślny Provider'),
+
+export const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+  const [isUserLogged, setIsUserLogged] = useState(false);
+  // const [isUserAdult, setIsUserAdult] = useState(true);
+
+  const toggleLoggedState = () => setIsUserLogged((prevValue) => !prevValue);
+  // const toggleAdultState = () => setIsUserAdult((prevValue) => !prevValue);
+
+  return (
+  <AppContext.Provider value={{
+    isUserLogged,
+    toggleLoggedState,
+  }} >
+    {children}
+    </AppContext.Provider>
+    )
+
 };
 
-export const AppContext = createContext(defaultObject);
+export default AppProvider;
